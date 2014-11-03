4d12c82d92544673bd4d7e44a314112c05cd6f46
Dan Fox <dan.fox@laterooms.com>
2014-11-03 11:32:53 +0000

    Fixed mongo version number

--- provisioning/install-mongodb.sh
+++ provisioning/install-mongodb.sh
@@ -3,5 +3,5 @@
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee /etc/apt/sources.list.d/mongodb.list
 apt-get update
-apt-get install -y mongodb-10gen
+apt-get install -y mongodb-org=2.6.5 mongodb-org-server=2.6.5 mongodb-org-shell=2.6.5 mongodb-org-mongos=2.6.5 mongodb-org-tools=2.6.5
 exit 0
